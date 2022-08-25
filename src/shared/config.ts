d46776195bb49b958f07b86c421025bb7fcc0b00
彭智鑫 <pengzhixin@live.cn>
2022-08-25 14:59:05 +0800

    fix: update icp

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -2,7 +2,7 @@ export default {
   name: '彭智鑫',
   title: '彭智鑫 - For Yumo & Lin',
   description: '记录生活、记录成长',
-  icp: '粤ICP备19128351号',
+  icp: '粤ICP备19128351号-2',
   website: 'https://pengyumo.com',
   email: 'pengzhixin@live.cn',
   avatar: {
