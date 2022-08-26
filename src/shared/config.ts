daddd26c07760c41a198fb9da0929416a9f0e887
彭智鑫 <pengzhixin@live.cn>
2022-08-26 16:35:31 +0800

    feat: update title

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,6 +1,6 @@
 export default {
-  name: '彭智鑫',
-  title: '彭智鑫 - For Yumo & Lin',
+  name: '服务端技术',
+  title: '服务端技术',
   description: '记录生活、记录成长',
   icp: '粤ICP备19128351号-2',
   website: 'https://pengyumo.com',
